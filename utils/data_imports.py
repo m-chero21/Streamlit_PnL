812743d71e04631afd8880f36da6a97e27746b68
Michelle Cherotich Kiprono <michelle.cherotich21@gmail.com>
2025-02-07 10:56:05 +0300

    Forward Slash

--- utils/data_imports.py
+++ utils/data_imports.py
@@ -2,15 +2,15 @@ import pandas as pd
 import streamlit as st
 @st.cache_data
 def load_csv_data():
-    return pd.read_csv("data\country - county - seed - data.csv")
+    return pd.read_csv("data/country - county - seed - data.csv")
 
 @st.cache_data
 def load_excel_data():
-    return pd.read_excel("data\country - county - crop - yield.xlsx")
+    return pd.read_excel("data/country - county - crop - yield.xlsx")
 
 @st.cache_data
 def load_cost_data():
-    return pd.read_excel("data\country - farmer - costs.xlsx")
+    return pd.read_excel("data/country - farmer - costs.xlsx")
 
 # Load datasets
 df = load_csv_data()
